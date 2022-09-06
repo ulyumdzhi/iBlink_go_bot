3a1e23a35665887a255ae70b494666c3ae9d75cf
ulyumdzhi <ulyumdzhiev@gmail.com>
2022-09-06 16:26:09 +0300

    init go bot

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+
+	const token string = "5566730175:AAFJ8IvuND_PywIut4MM7Qm8n4fN-Gk-Qik"
+	const timeToBlink = 42 // send message to straighten your back and blink every 42 minutes
+	const msgText string = "Выпрями спину и закрой глаза на 10 секунд"
+
+	bot, err := tgbotapi.NewBotAPI(token)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			for range time.Tick(time.Minute * timeToBlink) {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				bot.Send(msg)
+			}
+		}
+	}
+}
